8754c8ac2cc5530528ff27beef61d038656266ca
alexpado <register@alexpado.fr>
2020-02-23 00:59:35 +0100

    Fix Water Breathing at reload / relog (again)

--- src/main/java/fr/alexpado/minecraft/OceanicPlayer.java
+++ src/main/java/fr/alexpado/minecraft/OceanicPlayer.java
@@ -33,7 +33,7 @@ public class OceanicPlayer implements Runnable {
     private BossBar bar;
     private int waterBreathingRunningFor = 0;
     private int lastWaterBreathingDuration = 0;
-    private boolean applySpawnWaterBreathing = true;
+    private boolean applySpawnWaterBreathing = false;
 
     public OceanicPlayer(OceanicMemory oceanicMemory, Player player) {
         this.player = player;
