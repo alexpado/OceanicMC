bd6a25a304c4111eda2b4e7618904fe3b2be6096
alexpado <register@alexpado.fr>
2020-02-23 16:52:07 +0100

    WaterBreathingBar: Added sounds and color change when low on time remaining

--- src/main/java/fr/alexpado/minecraft/tools/WaterBreathingBar.java
+++ src/main/java/fr/alexpado/minecraft/tools/WaterBreathingBar.java
@@ -3,12 +3,15 @@ package fr.alexpado.minecraft.tools;
 import fr.alexpado.minecraft.Oceanic;
 import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
+import org.bukkit.Sound;
 import org.bukkit.boss.BarColor;
 import org.bukkit.boss.BarStyle;
 import org.bukkit.boss.BossBar;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+
+import java.util.concurrent.TimeUnit;
 
 public class WaterBreathingBar {
 
@@ -46,6 +49,7 @@ public class WaterBreathingBar {
     public void stop() {
         this.lastDurationTick = 0;
         this.totalDuration = 0;
+        this.player.playSound(this.player.getLocation(), Sound.ENTITY_ARROW_HIT_PLAYER, 0.1f, 0f);
         this.hide();
     }
 
@@ -58,6 +62,28 @@ public class WaterBreathingBar {
             }
             this.lastDurationTick = potionEffect.getDuration();
             this.bar.setProgress((double) potionEffect.getDuration() / (double) this.totalDuration);
+
+            String remainingTime = String.format("%02d:%02d",
+                    TimeUnit.SECONDS.toMinutes(potionEffect.getDuration() / 20),
+                    TimeUnit.SECONDS.toSeconds(potionEffect.getDuration() / 20) -
+                            TimeUnit.MINUTES.toSeconds(TimeUnit.SECONDS.toMinutes(potionEffect.getDuration() / 20))
+            );
+
+            this.bar.setTitle("Water Breathing (" + remainingTime + ")");
+
+            if (potionEffect.getDuration() <= 120) {
+                this.bar.setColor(BarColor.RED);
+                if (potionEffect.getDuration() % 20 == 0) {
+                    this.player.playSound(this.player.getLocation(), Sound.ENTITY_ARROW_HIT_PLAYER, 0.2f, 1.5f);
+                }
+            } else if (potionEffect.getDuration() == 200) {
+                this.player.playSound(this.player.getLocation(), Sound.ENTITY_ARROW_HIT_PLAYER, 0.2f, 1.2f);
+                this.bar.setColor(BarColor.YELLOW);
+            } else if (potionEffect.getDuration() <= 199) {
+                this.bar.setColor(BarColor.YELLOW);
+            } else {
+                this.bar.setColor(BarColor.BLUE);
+            }
         }
     }
 
